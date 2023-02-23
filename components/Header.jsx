746221aed8f87f965d3191bd00618c5ca922a70d
MrDeft <abouirjan@gmail.com>
2023-02-23 21:09:13 +0500

    header edit

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Blog CMS
+              Blog
             </span>
           </Link>
         </div>
